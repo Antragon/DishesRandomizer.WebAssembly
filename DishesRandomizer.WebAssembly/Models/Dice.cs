b1f8c5d362b429e425a1e6a18b3f730870d863f9
Antragon <unity@eilix.de>
2022-07-05 19:10:46 +0200

    Animate dice

---
+++ DishesRandomizer.WebAssembly/Models/Dice.cs
@@ -0,0 +1,14 @@
+﻿namespace DishesRandomizer.WebAssembly.Models;
+
+public class Dice {
+    public int Rotation { get; private set; }
+
+    public void RotateRandomly() {
+        var random = new Random();
+        Rotation = random.Next(0, 361);
+    }
+
+    public void Reset() {
+        Rotation = 0;
+    }
+}
